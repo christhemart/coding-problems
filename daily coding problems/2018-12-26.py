1d14894fd192b80d36ace94528c4369376f4df58
Chris Martinez <christhemart@gmail.com>
2018-12-27 23:03:35 -0600

    Add 12-26 and 12-27

---
+++ daily coding problems/2018-12-26.py
@@ -0,0 +1,59 @@
+'''
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Google.
+
+A unival tree (which stands for "universal value") is a tree where all nodes under it have the same value.
+
+Given the root to a binary tree, count the number of unival subtrees.
+
+For example, the following tree has 5 unival subtrees:
+
+   0
+  / \
+ 1   0
+    / \
+   1   0
+  / \
+ 1   1
+ '''
+
+class BinaryTree:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def unival_count(tree, parent):
+    count = 0
+
+    if tree.left is None and tree.right is None:
+        return 1, True if tree.value == parent.value else False
+    else:
+        unival = True
+
+        if tree.left is not None:
+            n, matching = unival_count(tree.left, tree)
+            count += n
+            unival = unival and matching
+
+        if tree.right is not None:
+            n, matching = unival_count(tree.right, tree)
+            count += n
+            unival = unival and matching
+
+        if unival is True:
+            count += 1
+
+        return count, True if tree.value == parent.value and unival is True else False
+
+
+my_tree = BinaryTree(0,
+    BinaryTree(1),
+    BinaryTree(0,
+        BinaryTree(1,
+            BinaryTree(1),
+            BinaryTree(1)),
+        BinaryTree(0)))
+assert unival_count(my_tree, my_tree)[0] == 5
---
+++ daily coding problems/2018-12-27.py
@@ -0,0 +1,24 @@
+'''
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Airbnb.
+
+Given a list of integers, write a function that returns the largest sum of non-adjacent numbers. Numbers can be 0 or negative.
+
+For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
+
+Follow-up: Can you do this in O(N) time and constant space?
+'''
+
+def nadj_max_sum(arr):
+    inclusive = 0
+    exclusive = 0
+    for i in arr:
+        new_exclusive = max(exclusive, inclusive)
+        inclusive = exclusive + i
+        exclusive = new_exclusive
+    return max(exclusive, inclusive)
+
+
+assert nadj_max_sum([2, 4, 6, 2, 5]) == 13
+assert nadj_max_sum([5, 1, 1, 5]) == 10
